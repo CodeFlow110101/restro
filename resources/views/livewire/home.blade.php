4033a07ae8494592fca3509c20227644f69592f6
codeflow <codeflow110101@gmail.com>
2025-05-10 13:01:11 +0530

    no change

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -18,10 +18,10 @@ use function Livewire\Volt\{state};
             <div x-data="{ show : false }" x-intersect:enter="show = true" x-intersect:leave="show = false" class="flex flex-col items-center gap-8">
                 <div :class="show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-full'" class="text-white text-center flex flex-col items-center gap-2 sm:gap-2.5 transition-all duration-[1500ms]">
                     <div class="sm:text-xl font-light tracking-wide">
-                        The Ancient Tradition of Hospitality
+                        Classic tradition of gracious hosting
                     </div>
-                    <div class="font-mrs-eaves text-5xl sm:text-7xl tracking-widest">
-                        PHILOXENIA
+                    <div class="font-mrs-eaves text-5xl sm:text-7xl tracking-widest uppercase">
+                        graciousness
                     </div>
                     <div class="border-t border-white w-2/12"></div>
                 </div>
@@ -34,7 +34,7 @@ use function Livewire\Volt\{state};
     <div class="py-14 flex justify-center">
         <div class="w-4/5 sm:w-11/12 flex flex-col gap-24">
             <div x-data="{ show : false }" x-intersect:enter="show = true" x-intersect:leave="show = false" :class="show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'" class="text-center font-light sm:text-lg transition-all duration-[1000ms]">
-                The Best Greek restaurant in Dubai, Milos is devoted to cooking fish perfectly, with artisan precision. Where sharing is our tradition and the finest ingredients are harnessed by chefs to create masterpieces.
+                The best restaurant dedicated on delivering exceptional dishes.Where community is built around the table Only the finest ingredients are used to craft unforgettable dishes
             </div>
             <div x-data="{ show : false }" x-intersect:enter="show = true" x-intersect:leave="show = false" :class="show ? '*:opacity-100 *:translate-y-0' : '*:opacity-0 *:translate-y-8'" class="border border-accent py-8 flex flex-col gap-2 sm:gap-4 items-center *:transition-all *:duration-[1000ms]">
                 <div class="text-center font-mrs-eaves text-accent text-3xl sm:text-4xl uppercase">Opening Hours</div>
@@ -58,17 +58,11 @@ use function Livewire\Volt\{state};
         <div class="flex max-sm:flex-col-reverse max-sm:gap-12 *:flex-1 *:grow w-full sm:h-dvh">
             <div x-data="{ show : false }" x-intersect:enter="show = true" x-intersect:leave="show = false" :class="show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-1/4'" class="flex flex-col gap-7 items-center justify-center transition-all duration-[1000ms]">
                 <div class="font-mrs-eaves text-3xl sm:text-4xl font-light uppercase text-center w-4/5 sm:w-11/12">
-                    Business Lunch At Estiatorio Milos
+                    Discuss, Dine, and Finalise the Deal at
                 </div>
-                <div class="border-t w-1/12"></div>
-                <div class="font-light text-center w-4/5 sm:w-11/12 sm:text-lg">
-                    Indulge in a culinary voyage with a delectable three-course lunch experience showcasing the world's most exquisite Mediterranean seafood.
-                </div>
-                <div class="border-t w-1/12"></div>
                 <div class="flex flex-col gap-2 items-center *:last:font-light font-bold sm:text-lg tracking-wide">
                     <div>Monday – Friday</div>
                     <div>12pm – 4pm</div>
-                    <div>AED 200</div>
                     <div class="underline">View Menu</div>
                 </div>
                 <div>
@@ -84,7 +78,7 @@ use function Livewire\Volt\{state};
     <div class="py-64 sm:py-72 flex justify-center items-center relative">
         <div class="flex flex-col items-center w-full gap-2 text-white">
             <div class="text-4xl uppercase font-mrs-eaves">
-                Our Gastronomy
+                Our Flavors
             </div>
             <div class="border-t border-white w-1/12"></div>
         </div>
@@ -94,7 +88,7 @@ use function Livewire\Volt\{state};
                 Your browser does not support the video tag.
             </video>
         </div>
-        <div class="absolute inset-0 -z-10 bg-gradient-to-t from-deep-ocean to-transparent"></div>
+        <div class="absolute inset-0 -z-10 bg-gradient-to-t from-deep-ocean via-transparent to-white"></div>
     </div>
     <div x-data="{ height: 0 }" class="py-14 flex justify-center max-sm:bg-deep-ocean sm:bg-gradient-to-t sm:from-deep-ocean/50 sm:to-deep-ocean to-20% relative">
         <div class="flex max-sm:flex-col *:flex-1 gap-12 sm:gap-24 w-[98%]">
@@ -104,10 +98,10 @@ use function Livewire\Volt\{state};
                 </div>
                 <div class="grow flex flex-col justify-center gap-2 w-11/12 sm:hidden mx-auto">
                     <div class="font-mrs-eaves text-accent text-2xl sm:text-3xl uppercase">
-                        Begins At Sea
+                        Tides of Taste
                     </div>
                     <div class="text-white font-light sm:text-lg">
-                        The ancient customs of early mornings and tremendous hard work underpin the excellence of fish and seafood, from the fisherman's boat to the chef's kitchen.
+                        From Dawn's Early Light to Dinner Plate
                     </div>
                 </div>
                 <div x-resize="height = $height" class="h-[80vh] w-11/12 ml-auto flex items-center justify-end sm:hidden">
@@ -115,10 +109,10 @@ use function Livewire\Volt\{state};
                 </div>
                 <div class="flex flex-col gap-2 sm:w-3/4 sm:pt-40 sm:pb-72 max-sm:w-11/12 max-sm:mx-auto">
                     <div class="font-mrs-eaves text-accent text-2xl sm:text-3xl uppercase">
-                        Earth’s Raw Materials
+                        Nature's Pantry
                     </div>
                     <div class="text-white font-light text-lg">
-                        Ingredients are the canvas upon which the chef paints a masterpiece, the building blocks giving each dish its unique character and flair.
+                        Where quality ingredients meet culinary creativity and masterpieces are born
                     </div>
                 </div>
                 <div :style="'height: ' + height*0.6 + 'px'" class="overflow-clip flex items-center w-4/5">
@@ -128,10 +122,10 @@ use function Livewire\Volt\{state};
             <div class="flex flex-col">
                 <div class="grow flex flex-col justify-center gap-2 w-3/4 max-sm:hidden">
                     <div class="font-mrs-eaves text-accent text-3xl uppercase">
-                        Begins At Sea
+                        Tides of Taste
                     </div>
                     <div class="text-white font-light text-lg">
-                        The ancient customs of early mornings and tremendous hard work underpin the excellence of fish and seafood, from the fisherman's boat to the chef's kitchen.
+                        From Dawn's Early Light to Dinner Plate
                     </div>
                 </div>
                 <div class="w-full">
@@ -139,10 +133,10 @@ use function Livewire\Volt\{state};
                 </div>
                 <div class="grow flex flex-col justify-center gap-2 mx-auto w-11/12 sm:w-3/4">
                     <div class="font-mrs-eaves text-accent text-2xl sm:text-3xl uppercase">
-                        Artisan Precision
+                        Refined Flavors
                     </div>
                     <div class="text-white font-light sm:text-lg">
-                        Our philosophy is simple. We are devoted to cooking fish perfectly. It is our life’s work.
+                        For us, cooking isn't just about following a recipe – it's about pouring our hearts and souls into every detail, to create truly unforgettable experiences
                     </div>
                 </div>
             </div>
@@ -160,12 +154,12 @@ use function Livewire\Volt\{state};
         </div>
         <div x-data="{ show : false }" x-intersect:enter="show = true" x-intersect:leave="show = false" :class="show ? '*:opacity-100 *:translate-y-0' : '*:opacity-0 *:translate-y-32'" class="size-full overflow-clip bg-black/50 flex flex-col gap-4 pt-44 justify-center items-center *:transition-all *:duration-[1000ms]">
             <div class="font-mrs-eaves text-5xl sm:text-7xl text-white text-center uppercase">
-                Where <br> Sharing Is <br> Our <br> Tradition
+                Sharing meals, <br> sharing memories
             </div>
             <div class="border-t w-1/12 border-white">
             </div>
             <div class="sm:w-1/2  text-white font-light text-center">
-                Sharing is an enduring tradition that connects us together, it's a timeless practice that transcends cultural boundaries, enriching our lives with the warmth of togetherness and the joy of generosity.
+                Love is a universal language that speaks to all, it's a timeless practice that bridges gaps, enriching our lives with joy and connection
             </div>
         </div>
     </div>
@@ -177,11 +171,11 @@ use function Livewire\Volt\{state};
         <div class="flex items-center">
             <div x-data="{ show : false }" x-intersect:enter="show = true" x-intersect:leave="show = false" :class="show ? '*:opacity-100 *:translate-y-0' : '*:opacity-0 *:translate-y-32'" class="flex flex-col items-center text-center gap-8 *:transition-all *:duration-[1000ms]">
                 <div class="font-mrs-eaves text-4xl uppercase">
-                    Taste Through Simplicity
+                    Where Flavors Speak
                 </div>
                 <div class="border-t w-1/12"></div>
                 <div class="w-11/12 font-light sm:text-lg">
-                    The Mediterranean has a wonderfully simple charm that goes beyond the crystal-clear water and the unique architecture. Nothing captures this better than the delicious simplicity of the Greek cuisine. Welcome to Milos.
+                    The Ocean has an effortless elegance that extends far beyond its stunning shores and breath taking views. Nothing embodies this better than the flavors that bring it to life
                 </div>
                 <div class="border-t w-1/12"></div>
             </div>
@@ -190,26 +184,26 @@ use function Livewire\Volt\{state};
     <div class="py-14 flex flex-col items-center gap-10">
         <div class="flex flex-col items-center gap-6 w-11/12">
             <div class="font-mrs-eaves uppercase text-4xl tracking-wide">
-                Menus
+                CUISINES
             </div>
             <div class="border-t w-12"></div>
         </div>
         <div x-data="{ show : false }" x-intersect:enter="show = true" x-intersect:leave="show = false" class="w-11/12 *:h-[50vh] flex gap-2 max-sm:*:shrink-0 max-sm:overflow-x-auto max-sm:snap-x snap-mandatory max-sm:*:snap-center *:overflow-clip *:size-full *:*:size-full *:relative">
             <div class="group">
                 <div :class="show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'" class="absolute z-10 inset-0 bg-gradient-to-t from-black/70 to-transparent uppercase font-mrs-eaves flex justify-center items-end text-white p-4 text-2xl transition-all duration-[1000ms]">
-                    food menu
+                    Local Specialties
                 </div>
                 <img class="group-hover:scale-125 transition-all duration-700" src="{{ asset('/images/home7.webp') }}">
             </div>
             <div class="group">
                 <div :class="show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'" class="absolute z-10 inset-0 bg-gradient-to-t from-black/70 to-transparent uppercase font-mrs-eaves flex justify-center items-end text-white p-4 text-2xl transition-all duration-[1000ms]">
-                    dessert menu
+                    Sweet Indulgence
                 </div>
                 <img class="group-hover:scale-125 transition-all duration-700" src="{{ asset('/images/home8.webp') }}">
             </div>
             <div class="group">
                 <div :class="show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'" class="absolute z-10 inset-0 bg-gradient-to-t from-black/70 to-transparent uppercase font-mrs-eaves flex justify-center items-end text-white p-4 text-2xl transition-all duration-[1000ms]">
-                    cocktail menu
+                    Spirits & Mixers
                 </div>
                 <img class="group-hover:scale-125 transition-all duration-700" src="{{ asset('/images/home9.webp') }}">
             </div>
@@ -262,10 +256,10 @@ use function Livewire\Volt\{state};
         <div class="flex flex-col w-[98%]">
             <div class="flex flex-col gap-2 items-center text-center">
                 <div class="uppercase text-lg">
-                    Our Mediterranean
+                    MARINE MAJESTY
                 </div>
                 <div class="uppercase font-mrs-eaves text-4xl">
-                    Gastronomy Journey
+                    Flavors of Exploration
                 </div>
                 <div class="border-t w-1/6 sm:w-1/12"></div>
             </div>
@@ -275,18 +269,18 @@ use function Livewire\Volt\{state};
                         <img src="{{ asset('/images/home10.webp') }}">
                     </div>
                     <div class="flex flex-col gap-4 max-sm:mx-auto w-11/12 sm:w-4/5 sm:hidden">
-                        <div class="font-mrs-eaves text-accent text-2xl sm:text-4xl uppercase">The Origins</div>
+                        <div class="font-mrs-eaves text-accent text-2xl sm:text-4xl uppercase">Genesis</div>
                         <div class="sm:text-xl font-light">
-                            Always finding the best the sea has to offer from flavours of fresh seafood and citrus accents to aromas coming from the fertile earth.
+                            Bringing forth the essence of the ocean and land, from succulent seafood to flavorful harvests
                         </div>
                     </div>
                     <div class="overflow-clip w-11/12 ml-auto sm:hidden">
                         <img x-resize="height = $height" class="h-[60vh] w-full" src="{{ asset('/images/home11.webp') }}">
                     </div>
                     <div class="flex flex-col gap-4 max-sm:mx-auto w-11/12 sm:w-4/5 sm:py-48">
-                        <div class="font-mrs-eaves text-accent text-2xl sm:text-4xl uppercase">The Process</div>
+                        <div class="font-mrs-eaves text-accent text-2xl sm:text-4xl uppercase">CRAFTING </div>
                         <div class="sm:text-xl font-light">
-                            Giving birth to new flavours through traditional spirit and uncompromising standards of quality and uniqueness.
+                            Crafting innovative Delicacies with timeless tradition and unwavering commitment to quality
                         </div>
                     </div>
                     <div :style="'height: ' + height*0.6 + 'px'" class="overflow-clip flex justify-center items-center">
@@ -295,18 +289,18 @@ use function Livewire\Volt\{state};
                 </div>
                 <div class="flex flex-col justify-center">
                     <div class="flex flex-col gap-4 py-14 w-4/5 max-sm:hidden">
-                        <div class="font-mrs-eaves text-accent text-4xl uppercase">The Origins</div>
+                        <div class="font-mrs-eaves text-accent text-4xl uppercase">Genesis</div>
                         <div class="text-xl font-light">
-                            Always finding the best the sea has to offer from flavours of fresh seafood and citrus accents to aromas coming from the fertile earth.
+                            Bringing forth the essence of the ocean and land, from succulent seafood to flavorful harvests
                         </div>
                     </div>
                     <div class="overflow-clip max-sm:hidden">
                         <img x-resize="if($width > 60) height = $height" class="h-dvh w-full" src="{{ asset('/images/home11.webp') }}">
                     </div>
                     <div class="flex flex-col gap-4 sm:py-14 max-sm:mx-auto w-11/12 sm:w-4/5">
-                        <div class="font-mrs-eaves text-accent text-2xl sm:text-4xl uppercase">The Experience </div>
+                        <div class="font-mrs-eaves text-accent text-2xl sm:text-4xl uppercase">Adventure</div>
                         <div class="sm:text-xl font-light">
-                            Following an edict of perfect simplicity to preserve each ingredient’s distinct flavour and nutritional value.
+                            Rooted in a commitment to quality, we ensure every element retains its natural essence and nutritional value
                         </div>
                     </div>
                 </div>
@@ -334,10 +328,10 @@ use function Livewire\Volt\{state};
     <div class="pt-14 pb-8 bg-black/5 flex flex-col items-center gap-12">
         <div class="flex items-center w-11/12 sm:*:flex-1 max-sm:flex-col max-sm:justify-start max-sm:gap-4">
             <div class="font-mrs-eaves uppercase text-3xl sm:text-4xl max-sm:w-full">
-                Stay In Touch
+                Stay Connected
             </div>
             <div class="font-light flex justify-start sm:text-lg">
-                Sign up to to be the first to know about our special offers.
+                Join our VIP list for exclusive perks.
             </div>
             <div class="flex sm:justify-end max-sm:w-full">
                 <button class="cursor-pointer border uppercase w-min border-black text-black hover:bg-black hover:text-white max-sm:text-sm py-4 px-20 sm:px-12 transition-all duration-500 whitespace-nowrap">Sign up</button>
